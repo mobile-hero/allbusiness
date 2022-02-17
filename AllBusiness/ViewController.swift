06e345dbe2272082e626b7a8d2a043dc1dcac2c0
Muhammad Ikhsan <enfinity.ikhsan@gmail.com>
2022-02-17 21:58:08 +0700

    Initial Commit

---
+++ AllBusiness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllBusiness
+//
+//  Created by Muhammad Ikhsan on 17/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
